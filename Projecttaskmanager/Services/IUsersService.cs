f9e52a7d28d969b439bd6db23294b3cf0e902b3b
tulasi179 <tulasi.nalluri@winwire.com>
2026-03-20 05:30:19 +0530

    adding frontend and backend

---
+++ Projecttaskmanager/Services/IUsersService.cs
@@ -0,0 +1,15 @@
+using System.Reflection.PortableExecutable;
+using Projecttaskmanager.Models;
+using Projecttaskmanager.DTOs;
+
+namespace Projecttaskmanager.Services;
+
+public interface IUsersService
+{
+    Task<List<UserResponce>> GetAllUsersAsync();
+    Task<UserResponce?> GetUserByIdAsync(int id);
+    Task<Users> AddUsersAsync(Users users);
+    Task<bool> UpdateUserAysnc(int id, UserResponce dto);
+    Task<bool> DeleteUsersAysnc(int id);
+
+}
